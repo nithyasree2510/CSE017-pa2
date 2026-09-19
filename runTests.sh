f79170fd096cf90528be1191909ebc3f0a7ee714
Nithya Sree Nagarajan <67371832+nithyasree2510@users.noreply.github.com>
2026-09-19 15:12:55 -0400

    Delete runTests.sh

--- runTests.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-rm -f output 2>&1 >/dev/null
-javac *.java
-rm accounts.txt
-cp original.txt accounts.txt
-java BankManager > output
-javac Testing.java
-java Testing 40 output.reference output
